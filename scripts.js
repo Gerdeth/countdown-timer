426997624d1f182fdb9f989fed808b17c7978fb4
Gerdeth Wickham-Daniels <gerdethwickham@gmail.com>
2020-11-24 06:42:54 -0500

    initial commit

---
+++ scripts.js
@@ -0,0 +1,22 @@
+let countDown;
+
+function timer(seconds){
+   const now= Date.now();
+   const then = now +seconds*1000;
+   displayTimeLeft(seconds);
+
+   
+    countDown= setInterval(()=>{
+       const secondsLeft = Math.round((then-Date.now())/1000)
+       if (secondsLeft < 0){
+           clearInterval(countDown);
+           return;
+       }
+    displayTimeLeft(secondsLeft);
+   }, 1000)
+}
+
+function displayTimeLeft(seconds){
+    console.log(seconds);
+
+}
