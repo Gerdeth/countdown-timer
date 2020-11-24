a50480ea7fedf1014efd5709f3cb0cf2d4044cff
Gerdeth Wickham-Daniels <gerdethwickham@gmail.com>
2020-11-24 07:23:02 -0500

    add event listeners

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 let countDown;
 const timerDisplay = document.querySelector('.display__time-left');
 const endTime = document.querySelector ('.display__end-time');
+const buttons = document.querySelectorAll('[data-time]')
 
 function timer(seconds){
    const now= Date.now();
@@ -31,5 +32,11 @@ function displayEndTime(timestamp){
     const end =new Date(timestamp);
     const hour = end.getHours();
     const minutes = end.getMinutes();
-    endTime.textContent = `Be Back At ${hour > 12 ? hour - 12:hour } ${minutes < 10? '0': ''}`;
+    endTime.textContent = `Be Back At ${hour > 12 ? hour - 12 :hour}:${minutes < 10? '0': ''}${minutes}`;
 }
+
+function startTimer(){
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+buttons.forEach(button => button.addEventListener('click',startTimer));
