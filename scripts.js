f6e481b5a2c7d1f83cfbc1baf87821d20f51fd50
Gerdeth Wickham-Daniels <gerdethwickham@gmail.com>
2020-11-24 06:53:09 -0500

    display time left

--- scripts.js
+++ scripts.js
@@ -1,11 +1,12 @@
 let countDown;
+const timerDisplay = document.querySelector('.display__time-left');
 
 function timer(seconds){
    const now= Date.now();
    const then = now +seconds*1000;
    displayTimeLeft(seconds);
 
-   
+
     countDown= setInterval(()=>{
        const secondsLeft = Math.round((then-Date.now())/1000)
        if (secondsLeft < 0){
@@ -17,6 +18,12 @@ function timer(seconds){
 }
 
 function displayTimeLeft(seconds){
+    const minutes= Math.floor(seconds/60);
+    const remainderSeconds = seconds%60;
+    const display =`${minutes}:${remainderSeconds < 10? '0': ''}${
+        remainderSeconds}`;
+    document.title = display
+    timerDisplay.textContent = display;
     console.log(seconds);
 
 }
